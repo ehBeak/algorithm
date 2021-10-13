c906f499ae1f3533d3d7863f255e12d83fe691b5
eunhee <dmsgml9903@gmail.com>
2021-10-13 16:21:32 +0900

    나이차 계산

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -1,26 +1,37 @@
 #include<iostream>
+#include<fstream>
+
 
 using namespace std;
 
 int main() {
+
+
 	/*
-	* 
+	*  cin을 반복해서 돌려도 버퍼에 있으니까 한번에 들어감
+	*  int형의 가장 작은 수  : -2147000000
+	*  int형의 가장 큰 수 : 2147000000
 	*/
-	// 자연수 n이 주어지면 자연수 n의 진약수의 합을 수식과 함께 출력
+	// N명의 나이중에 가장 나이차이 많이 나는 경우
+
+	// 파일 입력
+	/*ios_base::sync_with_stdio(false);
+	ifstream cin;
+	cin.open("input.txt");*/
 	// 변수
-	int n, i = 1, sum = i;
+	int n, min = 1000, max = 0, age;
 
 	// 입력
 	cin >> n;
 
-	cout << i++;
-	for (i ; i < n; i++) {
-		if (n % i == 0) {
-			cout << " + " << i;
-			sum += i;
-		}
+
+	for (int i = 0; i < n; i++) {
+		cin >> age;
+		if (min > age) { min = age; }
+		if (max < age) { max = age; }
 	}
 
-	cout << " = " << sum;
+	cout << max - min << endl;
+
 	return 0;
 }
