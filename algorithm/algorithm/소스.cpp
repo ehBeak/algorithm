35f1875570fc53870ad7d63b4f5b4066ddf14055
eunhee <dmsgml9903@gmail.com>
2021-10-14 20:37:41 +0900

    나이 계산(문자열 컨트롤)

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -1,16 +1,16 @@
-#include<iostream>
+#include<iostream> // <stdio.h>
 #include<fstream>
+#include<string>
 
 
 using namespace std;
 
 int main() {
 
-
+	 // "" : string, '':char
 	/*
-	*  cin을 반복해서 돌려도 버퍼에 있으니까 한번에 들어감
-	*  int형의 가장 작은 수  : -2147000000
-	*  int형의 가장 큰 수 : 2147000000
+	* 입력 주민등록 번호
+	* 출력 주인의 나이, 성별 판단
 	*/
 	// N명의 나이중에 가장 나이차이 많이 나는 경우
 
@@ -19,19 +19,42 @@ int main() {
 	ifstream cin;
 	cin.open("input.txt");*/
 	// 변수
-	int n, min = 1000, max = 0, age;
-
+	string no, first; // char a[20] string보다는 char 배열로
+	char second;
+	int age;
 	// 입력
-	cin >> n;
+	cin >> no; // scanf("%s", &a) // string으로 a의 참조값 읽음
 
-
-	for (int i = 0; i < n; i++) {
-		cin >> age;
-		if (min > age) { min = age; }
-		if (max < age) { max = age; }
+	second = no[7]; // 1900, 2000 구분
+	
+	if ((second == '1') || (second == '2')) { // 1900
+		first = "19";
+		// year = 1900 + ( (a[0]-48) * 10 + (a[1]-48)*1) // 48빼야지 정수형!!!
+	}
+	else if ((second == '3') || (second == '4')) { // 2000
+		first = "20";
 	}
 
-	cout << max - min << endl;
+	for (int i = 0; i < 2; i++) {
+		first = first + no[i];
+	}
+	
+	age = stoi(first);
+	age = 2019 - age + 1;
 
+	// printf("%d", age)
+
+	if ((second == '1') || (second == '3')) {
+		cout << age << " M" << endl;
+		
+	}
+	else if ((second == '2') || (second == '4')) {
+		cout << age << " W" << endl;
+	}
+
+	/*
+	* 
+	*/
+	
 	return 0;
 }
