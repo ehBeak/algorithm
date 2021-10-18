f5b641ebd7886e4678fdfe206cda5eb6000ca2bb
eunhee <dmsgml9903@gmail.com>
2021-10-18 22:17:19 +0900

    모두의 약수

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -1,37 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<fstream>
+//#include<fstream>
 
 
 using namespace std;
 
 int main() {
 
-
 	// 파일 입력
 	/*ios_base::sync_with_stdio(false);
 	ifstream cin;
 	cin.open("input.txt");*/
 
-	// 변수
-	char a[31];
-	int cnt = 0;
+	int a;
+	int cnt[50001] = { 0, };
 
-	scanf("%s", &a);
+	scanf("%d", &a);
 
-	for (int i = 0; a[i] != '\0'; i++) {
-		if (a[i] == '(') {
-			cnt++;
+
+	for (int i = 1; i <= a; i++) {
+		for (int j = i; j <= a; j = j + i) {
+			cnt[j]++;
 		}
-		else if (a[i] == ')') {
-			cnt--;
-		}
-
-		if (cnt < 0) break;
 	}
-	
-	if (cnt != 0) printf("NO");
-	else printf("YES");
 
+	for (int i = 1; i <= a; i++) {
+		printf("%d ", cnt[i]);
+	}
 	return 0;
 }
