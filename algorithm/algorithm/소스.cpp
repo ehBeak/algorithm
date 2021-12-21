7f0884552991a8c02b261022956b7bb2e07f6a42
eunhee <dmsgml9903@gmail.com>
2021-12-21 11:14:56 +0900

    22.온도의 최대값

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<vector>
 //#include<fstream>
 
 using namespace std;
@@ -12,44 +13,26 @@ int main() {
 	ifstream cin;
 	cin.open("input.txt");*/
 
-	int a[10], b[10];
-	int a_score = 0, b_score = 0;
-	int res;
+	
+	int n, k, tmp = 0, max = -2147483647;
+	//int a[100000] = { 0, };
 
-	for (int i = 0; i < 10; i++) {
+	scanf("%d %d", &n, &k);
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
 
-	for (int i = 0; i < 10; i++) {
-		scanf("%d", &b[i]);
-
-		if (a[i] > b[i]) {
-			a_score += 3;
-			res = 1;
-		}
-		else if (a[i] < b[i]) {
-			b_score += 3;
-			res = 2;
-		}
-		else if (a[i] == b[i]) {
-			a_score++; b_score++;
-		}
+	for (int i = 0; i < k; i++) {
+		tmp += a[i];
 	}
-	
+	max = tmp;
 
-	printf("%d %d\n", a_score, b_score);
-
-	if (a_score == b_score) {
-		if (res == 0) printf("D");
-		else if (res == 1) printf("A");
-		else if (res == 2)printf("B");
+	for (int i = k; i < n; i++) {
+		tmp = tmp + a[i] - a[i - k];
+		if (tmp > max) max = tmp;
 	}
-	
-	if (a_score > b_score) printf("A");
-	else printf("B");
 
-
-	
-
+	printf("%d", max);
 	return 0;
 }
