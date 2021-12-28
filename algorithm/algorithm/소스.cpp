35bbc8d87aa5c83217003c456db0614c2b2f4343
eunhee <dmsgml9903@gmail.com>
2021-12-28 11:21:17 +0900

    29.3의개수는?(large)

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -12,19 +12,29 @@ int main() {
 	/*ios_base::sync_with_stdio(false);
 	ifstream cin;
 	cin.open("input.txt");*/
-
-	int n, tmp, cnt = 0;
+	int n, cur, lt = 1, rt, k = 1, res = 0;
 	scanf("%d", &n);
+	
+	while (lt != 0) {
+		lt = n / k / 10;
+		cur = (n / k) % 10;
+		rt = n % k;
+		
+		//printf("lt: %d, cur: %d, rt: %d\n", lt, cur, rt);
 
-	//1234 123
-	for (int i = 1; i < n + 1; i++) {
-		tmp = i;
-		while (tmp > 1) {
-			if (tmp % 10 == 3) cnt++;
-			tmp = tmp/10;
-		}
+		if (cur == 3) res += lt * k + rt + 1;
+		else if (cur > 3) res += (lt + 1) * k;
+		else if (cur < 3) res += lt * k;
+
+		//printf("res : %d\n", res);
+
+		k *= 10;
 	}
-	printf("%d", cnt);
+
+	printf("%d", res);
+
+	
+
 	return 0;
 }
 
