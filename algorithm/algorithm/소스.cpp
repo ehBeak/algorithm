da024d9f04967a932103acf1fdbe0882b6982ef9
eunhee <dmsgml9903@gmail.com>
2021-12-30 10:55:56 +0900

    35. special sort(구글 인터뷰)

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -23,7 +23,7 @@ int main() {
 
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = 0; j < n - i - 1; j++) {
-			if (a[j] > a[j + 1]) {
+			if (a[j] > 0 && a[j + 1] < 0) {
 				tmp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = tmp;
@@ -31,7 +31,7 @@ int main() {
 		}
 	}
 
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 		printf("%d ", a[i]);
 
 	return 0;
