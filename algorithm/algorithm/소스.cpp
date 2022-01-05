4094b0c11ae94a184f36b704e9a0ddd4f22a4098
eunhee <dmsgml9903@gmail.com>
2022-01-05 11:29:49 +0900

    40.교집합_2(timelimit)

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -13,39 +13,51 @@ int main() {
 	ifstream cin;
 	cin.open("input.txt");*/
 
-	int n, m, idx = 0, tmp, cnt = 0, j;
+	int n, m, ai = 0, bi = 0, tmp, j;
 
 	scanf("%d", &n);
 	vector<int> a(n);
-	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-	
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+		tmp = a[i];
+		for (j = i - 1; j >= 0; j--) {
+			if (tmp > a[j]) break;
+			else a[j + 1] = a[j];
+		}
+		a[j + 1] = tmp;
+	}
+
 	scanf("%d", &m);
 	vector<int> b(m);
-	for (int i = 0; i < m; i++) scanf("%d", &b[i]);
-
-	vector<int> res(m + n - 1);
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (a[i] == b[j]) {
-				res[cnt++] = a[i];
-				break;
-			}
-		}
-	}
-	
-	for (int i = 1; i < cnt; i++) {
-		tmp = res[i];
+	for (int i = 0; i < m; i++) {
+		scanf("%d", &b[i]);
+		tmp = b[i];
 		for (j = i - 1; j >= 0; j--) {
-			if (tmp < res[j]) res[j + 1] = res[j];
-			else break;
+			if (tmp > b[j]) break;
+			else b[j + 1] = b[j];
 		}
-		res[j + 1] = tmp;
+		b[j + 1] = tmp;
 	}
 
-	for (int i = 0; i < cnt; i++) printf("%d ", res[i]);
-	
-	
+	while (true) {
+		
+		if (a[ai] == b[bi]) {
+			printf("%d ", a[ai]);
+			ai++;
+			bi++;
+		}
+		else if (a[ai] > b[bi]) {
+			bi++;
+		}
+		else if (a[ai] < b[bi]){
+			ai++;
+		}
+
+		if (ai >= n || bi >= m) break;
+
+	}
+
+
 	return 0;
 }
 
