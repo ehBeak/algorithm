e1d34799ae6865b1f67c1e8d2d86bd4da8aafd33
eunhee <dmsgml9903@gmail.com>
2022-01-06 11:52:48 +0900

    42.이분검색

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -13,25 +13,30 @@ int main() {
 	ifstream cin;
 	cin.open("input.txt");*/
 
-	int a, b = 1, cnt = 0, tmp;
-	scanf("%d", &a);
+	int n, key, lt = 0, rt, mid, tmp;
+	scanf("%d %d", &n, &key);
 
-	tmp = a;
+	rt = n - 1;
 
-	a--;
-	while (a > 0) {
-		b++;
-		a -= b;
-		if (a % b == 0) {
-			for (int i = 1; i < b; i++) {
-				printf("%d + ", i + (a / b));
-			}
-			printf("%d = %d\n", b + (a / b), tmp);
-			cnt++;
-		}
+	vector<int> a;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &tmp);
+		a.push_back(tmp);
 	}
 
-	printf("%d", cnt);
+	sort(a.begin(), a.end());
+
+	while (lt <= rt) {
+		mid = (lt + rt) / 2;
+
+		if (a[mid] == key) {
+			printf("%d", mid + 1);
+			return 0;
+		}
+		else if (a[mid] > key) rt = mid - 1;
+		else if (a[mid] < key) lt = mid + 1;
+	}
+
 	return 0;
 }
 
