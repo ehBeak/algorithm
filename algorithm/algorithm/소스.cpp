ea8413d011ade492054a9fde05a7ce34dd6fd155
eunhee <dmsgml9903@gmail.com>
2022-01-12 11:56:41 +0900

    43.뮤직비디오

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -5,6 +5,22 @@
 //#include<fstream>
 
 using namespace std;
+int a[1001], n;
+
+int count(int s) {
+	int sum = 0, cnt = 1;
+	for (int i = 0; i < n; i++) {
+		if (sum + a[i] > s) {
+			sum = a[i];
+			cnt++;
+		}
+		else {
+			sum += a[i];
+		}
+	}
+
+	return cnt;
+}
 
 int main() {
 
@@ -13,29 +29,30 @@ int main() {
 	ifstream cin;
 	cin.open("input.txt");*/
 
-	int n, key, lt = 0, rt, mid, tmp;
-	scanf("%d %d", &n, &key);
+	int m, res, lt = 1, rt = 0, mid, max = -2147000000;
 
-	rt = n - 1;
-
-	vector<int> a;
+	scanf("%d %d", &n, &m);
 	for (int i = 0; i < n; i++) {
-		scanf("%d", &tmp);
-		a.push_back(tmp);
+		scanf("%d", &a[i]);
+		rt += a[i];
+		if (a[i] >= max) max = a[i];
 	}
-
-	sort(a.begin(), a.end());
 
 	while (lt <= rt) {
-		mid = (lt + rt) / 2;
+		mid = (rt + lt) / 2;
 
-		if (a[mid] == key) {
-			printf("%d", mid + 1);
-			return 0;
+		if (mid >= max && count(mid) <= m) {
+			res = mid;
+			rt = mid - 1;
 		}
-		else if (a[mid] > key) rt = mid - 1;
-		else if (a[mid] < key) lt = mid + 1;
+		else {
+			lt = mid + 1;
+		}
+
 	}
+
+	printf("%d", res);
+	
 
 	return 0;
 }
