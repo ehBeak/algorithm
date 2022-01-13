6d98cd7274db667cc7f29ad97b5b2a646f7095ce
eunhee <dmsgml9903@gmail.com>
2022-01-13 11:09:48 +0900

    43.마구간 정하기_풀이

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -2,58 +2,47 @@
 #include<stdio.h>
 #include<vector>
 #include<algorithm>
-//#include<fstream>
 
 using namespace std;
-int a[1001], n;
+int a[200000], pre, now, n, c;
 
-int count(int s) {
-	int sum = 0, cnt = 1;
-	for (int i = 0; i < n; i++) {
-		if (sum + a[i] > s) {
-			sum = a[i];
+int count(int mid) {
+	int cnt = 1;
+	pre = a[0];
+
+	for (int i = 1; i < n; i++) {
+		now = a[i];
+		if (now - pre >= mid) {
+			pre = a[i];
 			cnt++;
 		}
-		else {
-			sum += a[i];
-		}
 	}
-
 	return cnt;
 }
 
 int main() {
 
-	// 파일 입력
-	/*ios_base::sync_with_stdio(false);
-	ifstream cin;
-	cin.open("input.txt");*/
+	int lt = 0, rt, mid, res;
+	scanf("%d %d", &n, &c);
+	
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
 
-	int m, res, lt = 1, rt = 0, mid, max = -2147000000;
-
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &a[i]);
-		rt += a[i];
-		if (a[i] >= max) max = a[i];
-	}
+	sort(a, a + n);
+	rt = a[n - 1];
 
 	while (lt <= rt) {
-		mid = (rt + lt) / 2;
 
-		if (mid >= max && count(mid) <= m) {
+		mid = (lt + rt) / 2;
+
+		if (count(mid) >= c) {
 			res = mid;
-			rt = mid - 1;
-		}
-		else {
 			lt = mid + 1;
 		}
-
+		else {
+			rt = mid - 1;
+		}
 	}
-
 	printf("%d", res);
-	
-
 	return 0;
 }
 
