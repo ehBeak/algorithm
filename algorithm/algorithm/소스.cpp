9d6f429d82595d7acd6ce4c88adad2bf0f3d188a
eunhee <dmsgml9903@gmail.com>
2022-03-10 17:44:03 +0900

    영지 선택(large)

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -11,30 +11,30 @@ int main() {
 	int H, W, h, w, sum = 0, res = 0;
 	scanf("%d %d", &H, &W);
 
-	vector<vector<int>> arr(H, vector<int>(W));
+	vector<vector<int>> arr(H+1, vector<int>(W+1));
+	vector<vector<int>> dy(H+1, vector<int>(W+1));
 
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < W; j++) {
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= W; j++) {
 			scanf("%d", &arr[i][j]);
 		}
 	}
-
 	scanf("%d %d", &h, &w);
 
-	for (int i = 0; i < H-h+1; i++) {
-		for (int j = 0; j < W-w+1; j++) {
-
-			for (int x = j; x < w+j; x++) {
-				for (int y = i; y < h+i; y++) {
-					sum += arr[y][x];
-				}
-			}
-			if (sum > res)res = sum;
-			sum = 0;
-			
-		}
-	 }
 	
+	for (int i = 1; i <= H; i++) {
+		for (int j = 1; j <= W; j++) {
+			dy[i][j] = arr[i][j] + dy[i][j - 1] + dy[i - 1][j] - dy[i - 1][j - 1];
+		}
+	}
+
+	for (int i = h; i <= H; i++) {
+		for (int j = w; j <= W; j++) {
+			sum = dy[i][j] - dy[i][j - w] - dy[i - h][j] + dy[i - h][j - w];
+			if (res < sum)res = sum;
+		}
+	}
+
 	printf("%d", res);
 
 	return 0;
