55f970b00bae4a90a46a3f1a936665ab1fa3d3e7
eunhee <dmsgml9903@gmail.com>
2022-03-12 15:43:14 +0900

    53. k진수 출력

--- algorithm/algorithm/소스.cpp
+++ algorithm/algorithm/소스.cpp
@@ -6,29 +6,31 @@
 
 using namespace std;
 
+int top = -1;
+int stack[100];
+
+void push(int e) {
+	stack[++top] = e;
+}
+
+int pop() {
+	return stack[top--];
+}
+
 int main() {
+	
+	int n, k;
+	scanf("%d %d", &n, &k);
+	char str[20] = "0123456789ABCDEF";
 
-	int n, p2, p3, p5, min = 214700000;
-	scanf("%d", &n);
-	vector<int> a(n, 1);
-
-	p2 = p3 = p5 = 0;
-	for (int i = 1; i < n; i++) {
-
-
-		if (a[p2] * 2 < a[p3] * 3) min = a[p2] * 2;
-		else min = a[p3] * 3;
-		if (a[p5] * 5 < min)min = a[p5] * 5;
-
-
-		if (a[p2] * 2 == min) p2++;
-		if (a[p3] * 3 == min) p3++;
-		if (a[p5] * 5 == min) p5++;
-
-		a[i] = min;
+	while (n != 0) {
+		push(n % k);
+		n /= k;
 	}
 
-	printf("%d", a[n-1]);
+	while (top != -1) {
+		printf("%c", str[pop()]);
+	}
 
 	return 0;
 }
